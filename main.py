21cdf75ab686485482fc6b5d01e017be5cdbaae7
Swaroop <135572982+SnappiBrawn@users.noreply.github.com>
2023-11-23 18:26:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,22 @@ async def on_message(message):
 async def on_member_join(member):
     log_channel = bot.get_channel(logschannel)
     await validateUsername(member)
-    await log_channel.send(f"DisplayName: {member.display_name} joined the server")
-    await member.send("Hi, I just noticed you joined our server. Here's a brief on teh server rules.")
+    await log_channel.send(f"{member.display_name} joined the server")
+    await member.send("Hi, I just noticed you joined our server. Here's a brief on the server rules.")
+    await member.send(""" Rules of the server:
+- Be respectful, civil, and welcoming.
+- No inappropriate or unsafe content.
+- Do not misuse or spam any of the channels.
+- No self-promotion, soliciting, or advertising.
+- NSFW content is not allowed.
+- Do not buy, sell, trade, or give away anything.
+- Do not use the server as a dating site.
+- Please communicate in English only.
+- Discord names and avatars must be appropriate.
+- Spamming in any form is not allowed.
+- Controversial topics like religion and politics are not allowed.
+- Do not attempt to bypass any blocked words.
+- Don’t ping without legitimate reasoning behind them.""")
 
 @bot.event
 async def on_member_update(before, after):
@@ -69,7 +83,7 @@ async def on_member_update(before, after):
 
 @bot.slash_command(name="clearchat")
 async def clear(ctx):
-  """CAUTION!! Clears all the chats in current channel CAUTION!!"""
+  """CAUTION!! Clears all the chats in current channel. CAUTION!!"""
   await ctx.channel.purge()
   await ctx.send("Channel cleared.")
 
@@ -94,15 +108,14 @@ async def rules(ctx):
 @bot.slash_command(name="hello")
 async def hello(ctx):
     """For any kind of rotaract assitance that you might need. """
-    await ctx.send(f"Hey there User! Please choose a what you need help with:\n"
+    await ctx.send(f"Hey there User! Please choose what you need help with:\n"
                    "1. About rotaract\n"
                    "2. About 3191\n"
-                   "3. About District Structure\n"
-                   "4. Details about my club")
+                   "3. About District Structure\n")
 
     # Wait for the user's choice
     try:
-        print(type(ctx.message))
+        # print(type(ctx.message))
         response = await bot.wait_for(
             'message',
             # check=lambda message: message.author == ctx.author and message.channel == ctx.channel,
@@ -116,20 +129,16 @@ async def hello(ctx):
     # Process the user's choice
     choice = response.content.lower()
     if choice == '1' or choice == 'about rotaract':
-        await ctx.send("Some brief info about Rotaract and rotary. might possiblly span 10-15 lines.",ephemeral=True)
+        await ctx.send("Rotaract, a global service organization for young adults aged 18 to 30, fosters leadership, fellowship, and community service. Affiliated with Rotary International, Rotaract clubs engage in local and international projects, emphasizing professional development and social responsibility while empowering individuals to create positive change in their communities worldwide.",ephemeral=True)
         # Implement logic for technical support inquiries
     elif choice == '2' or choice == 'about 3191':
-        await ctx.send("Some brief info about RID3191. DRR info, zones etc.",ephemeral=True)
+        await ctx.send("There's plenty to talk about 3191, why not head over to our [website](https://rotaract3191.org/) for some in-depth knowledge?",ephemeral=True)
         # Implement logic for billing inquiries
     elif choice == '3' or choice == 'about district structure':
-        await ctx.send("Administrative heirarchy. List of facilitators. District roles etc.",ephemeral=True)
-        # Implement logic for general inquiries
-    elif choice == '4' or choice == 'details about my club':
-        await ctx.send("further branching",ephemeral=True)
+        await ctx.send("All Rotaract Districts are represented by their DRRs (District Rotaract representatives). Each DRR has his own team who are responsible for the various levels of management. In 3191, the DRR’s core committee consists of the DRR – Elect, District Rotaract Secretaries (DRS’s) for Administration, Training and Initiatives, and Operations, and the District Joint Secretary (DJS). The various avenues of service are headed by the respective Directors from the Executive Council and their teammates from the District Committee. The District itself is divided into 4 administrative zones, each headed by two Zonal Rotaract Representatives (ZRRs). These ZRRs are assigned as the administrators of the clubs under their zones, led by the respective club Presidents. Further hierarchies are decided by these presidents. ",ephemeral=True)
         # Implement logic for general inquiries
     else:
         await ctx.send("Invalid choice. Please choose a valid option (1, 2, or 3).",ephemeral=True)
         return
 
-
-bot.run("${{secrets.YOUR_SECRET_NAME}}")
+bot.run("")
